39d79d8d560a765d6d2e8c282ca7060ebd149bf2
Derek Kozel <derek@bitstovolts.com>
2023-12-22 15:45:56 +0000

    Updated gr-eb200 to GNU Radio 3.10

--- MANIFEST.md
+++ MANIFEST.md
@@ -1,10 +1,17 @@
-title: gr-eb200
-brief: GNU Radio module to use the R&S EB200 protocol for IQ data
+title: The EB200 OOT Module
+brief: Short description of gr-eb200
 tags: # Tags are arbitrary, but look at CGRAN what other authors are using
   - sdr
-  - IQ
-  - protocol
 author:
-  - chris007 
-repo: https://github.com/dl0muc/gr-eb200 # Put the URL of the repository here, or leave blank for default
+  - Author Name <authors@email.address>
+copyright_owner:
+  - Copyright Owner 1
+license:
+gr_supported_version: # Put a comma separated list of supported GR versions here
+#repo: # Put the URL of the repository here, or leave blank for default
+#website: <module_website> # If you have a separate project website, put it here
+#icon: <icon_url> # Put a URL to a square image here that will be used as an icon on CGRAN
 ---
+A longer, multi-line description of gr-eb200.
+You may use some *basic* Markdown here.
+If left empty, it will try to find a README file instead.
--- lib/if_stream_decode_impl.h
+++ lib/if_stream_decode_impl.h
@@ -1,38 +1,24 @@
 /* -*- c++ -*- */
 /*
- * Copyright 2016 <+YOU OR YOUR COMPANY+>.
+ * Copyright 2023 gr-eb200 author.
  *
- * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #ifndef INCLUDED_EB200_IF_STREAM_DECODE_IMPL_H
 #define INCLUDED_EB200_IF_STREAM_DECODE_IMPL_H
 
-#include <eb200/if_stream_decode.h>
-
+#include <gnuradio/eb200/if_stream_decode.h>
 #include "eb200header.h"
 
 namespace gr {
-  namespace eb200 {
+namespace eb200 {
 
-    class if_stream_decode_impl : public if_stream_decode
-    {
-     private:
+class if_stream_decode_impl : public if_stream_decode {
+private:
       bool m_pDebugEnabled;
       bool m_pSynced;
+      int  previousSeqNumber;
       // Expected DataSize of the received packets, read from Common Header
       unsigned long m_pDataSize;
 
@@ -43,20 +29,19 @@ namespace gr {
       void print_eb200_header(EB200_HEADER_TYPE&);
       void print_udp_datagram_attribute(UDP_DATAGRAM_ATTRIBUTE_TYPE&);
 
-     public:
-      if_stream_decode_impl(bool debug);
-      ~if_stream_decode_impl();
+public:
+  if_stream_decode_impl(bool debug);
+  ~if_stream_decode_impl();
 
-      // Where all the action really happens
       void forecast (int noutput_items, gr_vector_int &ninput_items_required);
 
       int general_work(int noutput_items,
            gr_vector_int &ninput_items,
            gr_vector_const_void_star &input_items,
            gr_vector_void_star &output_items);
-    };
+};
 
-  } // namespace eb200
+} // namespace eb200
 } // namespace gr
 
 #endif /* INCLUDED_EB200_IF_STREAM_DECODE_IMPL_H */
